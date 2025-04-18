bef94dd5fafc965ccbd66976c9b6a6a344f8ba9e
ashikfifa <ashikahmedfifa@gmail.com>
2025-04-18 23:28:21 +0600

    load effect on the card

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -3,12 +3,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../store";
 import { useEffect } from "react";
 import { fetchDeliveries } from "../../features/deliveries/deliveriesSlice";
-import { Grid } from "@mui/material";
+import { CircularProgress, Grid } from "@mui/material";
 
 const Dashboard = () => {
   const statusCounts = useSelector(
     (state: RootState) => state.deliveries.statusCounts
   );
+  const status = useSelector((state: RootState) => state.deliveries.status);
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
@@ -20,23 +21,39 @@ const Dashboard = () => {
       <div className="deliveries">
         <p className="titleDelivery">Dashboard</p>
       </div>
-      <Grid container spacing={2}>
-        <Grid size={{ xs: 12, sm: 6, md: 3 }}>
-          <StatusCard label="Pending" count={statusCounts?.Pending} />
+
+      {status === "loading" ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "2rem",
+          }}
+        >
+          <CircularProgress />
+        </div>
+      ) : (
+        <Grid container spacing={2}>
+          <Grid size={{ xs: 12, sm: 6, md: 3 }}>
+            <StatusCard label="Pending" count={statusCounts?.Pending} />
+          </Grid>
+          <Grid size={{ xs: 12, sm: 6, md: 3 }}>
+            <StatusCard
+              label="In Transit"
+              count={statusCounts?.["In Transit"]}
+            />
+          </Grid>
+          <Grid size={{ xs: 12, sm: 6, md: 3 }}>
+            <StatusCard label="Delivered" count={statusCounts?.Delivered} />
+          </Grid>
+          <Grid size={{ xs: 12, sm: 6, md: 3 }}>
+            <StatusCard
+              label="Not Delivered"
+              count={statusCounts?.["Not Delivered"]}
+            />
+          </Grid>
         </Grid>
-        <Grid size={{ xs: 12, sm: 6, md: 3 }}>
-          <StatusCard label="In Transit" count={statusCounts?.["In Transit"]} />
-        </Grid>
-        <Grid size={{ xs: 12, sm: 6, md: 3 }}>
-          <StatusCard label="Delivered" count={statusCounts?.Delivered} />
-        </Grid>
-        <Grid size={{ xs: 12, sm: 6, md: 3 }}>
-          <StatusCard
-            label="Not Delivered"
-            count={statusCounts?.["Not Delivered"]}
-          />
-        </Grid>
-      </Grid>
+      )}
     </>
   );
 };
