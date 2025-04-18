ce9e81ac1ffd3ebc9590505b037a60aaf81ad304
ashikfifa <ashikahmedfifa@gmail.com>
2025-04-18 22:23:22 +0600

    add env

--- src/features/deliveries/deliveriesSlice.ts
+++ src/features/deliveries/deliveriesSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_URL = "https://delivertrack.onrender.com/deliveries";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3001/deliveries";
 // const API_URL= process.env.REACT_APP_API_URL
 
 type Delivery = {
